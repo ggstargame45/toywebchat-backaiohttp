1eefc2286fa2a14ad53fcdd49ac51b7bf8c9ed0c
ggstargame45 <ggstargame45@kookmin.ac.kr>
2024-06-28 14:50:35 +0900

    add ssl for https,wss

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from aiohttp import web
 import aiohttp_cors
-import os
 from app.presentation.routes import init_routes
 from app.presentation.websocket import websocket_handler
 from app.infrastructure.redis_repository import RedisRepository
 from app.application.services import ChatService
 
-#TODO : 외부에서 접속 가능하게 하기
+import ssl
+
 def create_app():
     app = web.Application()
     app['websockets'] = set()
 
-    redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
-    redis_repo = RedisRepository(redis_url=redis_url)
+    redis_repo = RedisRepository(redis_url='redis://localhost:6379')
     chat_service = ChatService(repository=redis_repo)
     app['chat_service'] = chat_service
 
@@ -36,4 +35,6 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    web.run_app(app, host='0.0.0.0', port=8080)
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(certfile='../certtest.pem', keyfile='../keytest.pem')
+    web.run_app(app, host='0.0.0.0', port=8080, ssl_context=ssl_context)
